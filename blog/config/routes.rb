607b77d3df47d03ba11d8f9251edd22e9358452d
Pol <pol@hotmail.com>
2014-07-09 18:02:44 +0200

    Second commit

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  # Pol's made
+  get "posts/new"
+  post "posts" => "posts#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
